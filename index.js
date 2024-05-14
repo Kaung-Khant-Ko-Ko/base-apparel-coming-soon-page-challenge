624eda23fe10ea1fd805fec1d078fddbcf0b5f6e
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-05-14 20:30:21 +0630

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// alert("Working");
+var item = $("#error-msg");
+var warning = $("#error");
+
+$("#btn").click(function () {
+  var input = $("#email-input").val();
+  if (input === "" || validate(input) === false) {
+    var msg = "";
+    if (input === "") {
+      msg = "Whoops! It looks like you forgot to add your email";
+    } else {
+      msg = "Please provide a valid email address";
+    }
+    $("#email-input").addClass("warning-box");
+
+    item.text(msg);
+    item.addClass("show-warning");
+    warning.addClass("show-warning");
+
+    item.fadeIn();
+    warning.fadeIn();
+
+    for (var count = 0; count < 3; count++) {
+      warning.fadeOut();
+      warning.fadeIn();
+    }
+  } else {
+    $("#email-input").removeClass("warning-box");
+    item.removeClass("show-warning");
+    warning.removeClass("show-warning");
+  }
+});
+
+function validate(email) {
+  var pattern = /^[\w]+[\.\w+]*[\w]+@[\w]+\.[\w]+[\.\w]*[\w]+$/;
+  return pattern.test(email);
+}
